9b6b568d1cbf407b840834836278fd3b3e999a2d
Siena Aguayo <siena.aguayo@gmail.com>
2020-09-17 10:34:59 -0700

    beginnings of an importer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,13 @@
 class HomeController < ApplicationController
   def index
     if Artist.count == 0
-      redirect_to import_path
+      render 'home/import'
     else
       redirect_to artists_index_path
     end
   end
 
   def import
+    Importer.start("")
   end
 end
