5e547840a642b6d33f5af7bf6f4d65672645d7a5
Siena Aguayo <siena.aguayo@gmail.com>
2020-09-19 17:41:39 -0700

    making stuff cute

--- app/javascript/packs/song_download.js
+++ app/javascript/packs/song_download.js
@@ -3,6 +3,26 @@ import $ from "jquery";
 $(document).ready(() => {
   $("#selectAll").change(function () {
     $(".checkbox-song").prop("checked", this.checked);
+
+    $(".checkbox-song").each(function () {
+      toggleRowSelected(this);
+    });
+  });
+
+  $(".checkbox-song").click(function (event) {
+    // don't let clicks bubble up to the row, since that has its own click handler
+    // that we don't want to trigger (since it changes the checkbox state)
+    event.stopPropagation();
+  });
+
+  $(".checkbox-song").change(function () {
+    toggleRowSelected(this);
+  });
+
+  $(".song-row").click(function () {
+    const checkbox = $(this).find(".checkbox-song");
+    checkbox.prop("checked", !checkbox.prop("checked"));
+    toggleRowSelected(checkbox);
   });
 
   $("#downloadButton").click(() => {
@@ -18,3 +38,11 @@ $(document).ready(() => {
     window.location = `${url}?${params}`;
   });
 });
+
+function toggleRowSelected(element) {
+  if ($(element).prop("checked")) {
+    $(element).parents("tr").addClass("tr-selected");
+  } else {
+    $(element).parents("tr").removeClass("tr-selected");
+  }
+}
